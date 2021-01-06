87c66d0898512a5c8fba15773e95a022172f9695
KoxSosen <whyconfused2@gmail.com>
2021-01-06 11:11:37 +0100

    Upload stuff

---
+++ src/fun/hahota/simon/Main.java
@@ -0,0 +1,33 @@
+package fun.hahota.simon;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// This plugin is always acive grrr
+		getLogger().info(ChatColor.GREEN + "You are now safe from reloading!");
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		
+	}
+
+	@EventHandler
+	public void onReloadCommand(PlayerCommandPreprocessEvent e) {
+	    String cmd = e.getMessage().split(" ")[0].replace("/", "").replaceAll("(?i)bukkit:", "");
+	    if(cmd.equalsIgnoreCase("reload") || cmd.equalsIgnoreCase("rl")) {
+	        e.setCancelled(true);
+	        Bukkit.shutdown();
+	        e.getPlayer().sendMessage("Server reloaded");
+	    }
+	}
+}
