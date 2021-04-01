41e48f34fd5ca171d80c438be65302f5d249da8e
KoxSosen <whyconfused2@gmail.com>
2021-04-01 19:45:52 +0200

    todo, up version

--- src/main/java/hahota/simon/fun/Simon/bungee/bungeereloadcommand.java
+++ src/main/java/hahota/simon/fun/Simon/bungee/bungeereloadcommand.java
@@ -1,10 +1,19 @@
 package hahota.simon.fun.Simon.bungee;
 
+import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
 public class bungeereloadcommand extends Plugin implements Listener {
+
+  //  @EventHandler
+   // public void onCommand(Command event) {
+     //   event.
+
+
+
+   // } todo
 
 
 }
