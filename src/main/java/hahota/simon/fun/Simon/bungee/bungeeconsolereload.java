6d3df17ec5918089535ffeded1c993ba6318856c
KoxSosen <whyconfused2@gmail.com>
2021-04-01 18:39:13 +0200

    Should work.

---
+++ src/main/java/hahota/simon/fun/Simon/bungee/bungeeconsolereload.java
@@ -0,0 +1,23 @@
+package hahota.simon.fun.Simon.bungee;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.event.ProxyReloadEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.command.PlayerCommand;
+import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
+
+public class bungeeconsolereload extends Plugin implements Listener {
+
+    @EventHandler (priority = EventPriority.HIGH)
+    public void onProxyReloadEvent(ProxyReloadEvent event) {
+        getLogger().info(ChatColor.RED + "I see you reloaded, let me help you with that!");
+        getProxy().stop();
+
+
+
+
+    }
+}
